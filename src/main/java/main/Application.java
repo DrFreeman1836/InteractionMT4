69cf404d999b61a8eeea50e8acf3bb72dee0ef51
viktoragafonov <Ser_Viktor1836@mail.ru>
2022-06-19 01:00:08 +0300

    Initial commit

---
+++ src/main/java/main/Application.java
@@ -0,0 +1,16 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableFeignClients
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
